51855324c268eaa2769c90dfb5ebf96986bde076
Enrico Asuncion <ejmasuncion@yahoo.com>
2026-03-23 22:05:46 +0800

    added test cases

---
+++ source/test_cases.py
@@ -0,0 +1,22 @@
+# source/test_cases.py
+import random
+
+class TestSequenceGenerator:
+    def __init__(self, num_blocks, words_per_block):
+        self.n = num_blocks
+        self.wpb = words_per_block
+
+    def get_sequential(self):
+        base_sequence = list(range(2 * self.n))
+        return base_sequence * 2
+
+    def get_mid_repeat(self): 
+        middle = list(range(0, self.n))
+        suffix = list(range(self.n, 2 * self.n))
+        
+        full_cycle = middle + middle + suffix
+        return full_cycle * 2
+
+    def get_random(self):
+        blocks = [random.randint(0, 63) for _ in range(20)]
+        return [b * self.wpb for b in blocks]
